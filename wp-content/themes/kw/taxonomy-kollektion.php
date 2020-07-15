ee2c3efe9e99f37a9fd2c38e78b2f148577cdb3d
cwirsching <c.wirsching@neusta>
2020-07-15 14:16:36 +0200

    Added new taxonomy template

--- wp-content/themes/kw/taxonomy-kollektion.php
+++ wp-content/themes/kw/taxonomy-kollektion.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <?php
-    $taxonomyTerm = get_query_var( 'kollektion' );
+    $taxonomySlug = get_query_var( 'kollektion' );
 
     $the_query = new WP_Query( array(
         'post_type' => 'produkte',
@@ -10,7 +10,7 @@
             array (
                 'taxonomy' => 'kollektion',
                 'field' => 'slug',
-                'terms' => $taxonomyTerm,
+                'terms' => $taxonomySlug,
             )
         )
     ) );
@@ -18,39 +18,29 @@
 
 <div class="collection-slider js-collection-slider">
     <?php while ( $the_query->have_posts() ) : $the_query->the_post();
-        $terms  = get_the_terms( $post->ID, 'kollektion' );
+        $kollektions  = get_the_terms( $post->ID, 'kollektion' );
 
-        if($terms) :
-            foreach ( $terms as $term ) : ?>
+        if($kollektions) :
+            foreach ( $kollektions as $kollektion ) : ?>
                 <?php
-                    $collectionName = $term->name;
-                    $collectionYear = get_field('collection_year', $term);
+                    $collectionYear = get_field('collection_year', $kollektion);
                     $productImagesDesktop = get_field('product_images_desktop');
                     $productImagesTablet = get_field('product_images_tablet');
                     $productImagesMobile = get_field('product_images_mobile');
                     $productDescription = get_field('product_description');
-                    $allProductImages = array();
-
-                    for($i = 0; $i < count($productImagesDesktop); $i++) {
-                        $allProductImages[$i]['Desktop'] = $productImagesDesktop[$i]['sizes']['large'];
-                        $allProductImages[$i]['Tablet'] = $productImagesTablet[$i]['sizes']['large'];
-                        $allProductImages[$i]['Mobile'] = $productImagesMobile[$i]['sizes']['large'];
-                        $allProductImages[$i]['Caption'] = $productImagesDesktop[$i]['caption'];
-                    }
                 ?>
 
-                <?php if( $allProductImages): ?>
-                    <?php for($x = 0; $x < count($allProductImages); $x++) : ?>
+                   <?php if($kollektion->slug === $taxonomySlug) : ?>
                         <div>
                             <span class="collection-name">
-                                <?php echo $collectionName . ' (' . $collectionYear . ')'; ?>
+                                <?php echo $kollektion->name . ' (' . $collectionYear . ')'; ?>
                             </span>
 
                             <picture>
-                                <source media="(min-width: 1024px)" srcset="<?php echo $allProductImages[$x]['Desktop']; ?>">
-                                <source media="(min-width: 768px)" srcset="<?php echo $allProductImages[$x]['Tablet']; ?>">
-                                <source media="(min-width: 320px)" srcset="<?php echo $allProductImages[$x]['Mobile']; ?>">
-                                <img class="img-item" src="<?php echo $allProductImages[$x]['Desktop']; ?>">
+                                <source media="(min-width: 1024px)" srcset="<?php echo $productImagesDesktop[0]['sizes']['large']; ?>">
+                                <source media="(min-width: 768px)" srcset="<?php echo $productImagesTablet[0]['sizes']['large']; ?>">
+                                <source media="(min-width: 320px)" srcset="<?php echo $productImagesMobile[0]['sizes']['large']; ?>">
+                                <img class="img-item" src="<?php echo $productImagesDesktop[0]['sizes']['large']; ?>">
                             </picture>
 
                             <span class="product-title">
@@ -58,23 +48,44 @@
                             </span>
 
                             <div class="product-information">
-                                <p class="img-caption"><?php echo $allProductImages[$x]['Caption']; ?></p>
+                                <p class="img-caption"><?php echo $productImagesDesktop[0]['Caption']; ?></p>
                             </div>
 
-                            <!--<div class="product-information">
-                                <a href="#information<?php echo $x; ?>" class="product-information js-product-information">
+                            <div class="product-information">
+                                <a href="#gallery<?php echo $index; ?>" class="product-gallery js-product-gallery">
+                                    Gallerie (<?php if( $productImagesDesktop ):  echo count($productImagesDesktop); endif; ?>),
+                                </a>
+                                <a href="#information<?php echo $index; ?>" class="product-information js-product-information">
                                     Informationen
                                 </a>
                             </div>
 
-                            <div id="information<?php echo $x; ?>" class="product-information-text mfp-hide">
+                            <div id="gallery<?php echo $index; ?>" class="js-product-information-gallery mfp-hide">
+                                <?php if( $productImagesDesktop ): ?>
+                                    <?php foreach ( $productImagesDesktop as $productDesktopImage ) : ?>
+                                        <div class="slide">
+                                            <picture>
+                                                <source media="(min-width: 1024px)" srcset="<?php echo $productDesktopImage['sizes']['large']; ?>">
+                                                <source media="(min-width: 768px)" srcset="<?php echo $productImagesTablet['sizes']['large'] ?>">
+                                                <source media="(min-width: 320px)" srcset="<?php echo $productImagesMobile['sizes']['large'] ?>">
+                                                <img src="<?php echo $productDesktopImage['sizes']['large'] ?>">
+                                            </picture>
+                                            <p class="img-caption"><?php echo $productImagesDesktop['Caption']; ?></p>
+                                        </div>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </div>
+
+                            <div id="information<?php echo $index; ?>" class="product-information-text mfp-hide">
                                 <div class="content">
                                     <?php echo $productDescription; ?>
                                 </div>
-                            </div>-->
+                            </div>
                         </div>
-                    <?php endfor; ?>
+                        <?php $index++; ?>
+                    <?php break; ?>
                 <?php endif; ?>
+
             <?php endforeach; ?>
         <?php endif; ?>
     <?php endwhile; ?>
