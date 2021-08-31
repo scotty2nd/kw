57722b93d17e12497bf038d4db6a661ae1f15f26
cwirsching <c.wirsching@neusta>
2021-08-31 00:00:07 -1000

    wp 5.8 and plugins updated

--- wp-content/plugins/post-types-order/include/class.cpto.php
+++ wp-content/plugins/post-types-order/include/class.cpto.php
@@ -267,6 +267,10 @@
                     //check if post type is sortable
                     if(isset($options['show_reorder_interfaces'][$screen->post_type]) && $options['show_reorder_interfaces'][ $screen->post_type ] != 'show')
                         return;
+                        
+                    //not for hierarhical
+                    //if ( is_post_type_hierarchical( $screen->post_type ) )
+                        //return; 
                     
                     //if is taxonomy term filter return
                     if(is_category()    ||  is_tax())
@@ -471,6 +475,8 @@
                             $data = apply_filters('pto/save-ajax-order', $data, $menu_order, $id);
                             
                             $wpdb->update( $wpdb->posts, $data, array('ID' => $id) );
+                            
+                            clean_post_cache( $id );
                         }
                         
                     //trigger action completed
